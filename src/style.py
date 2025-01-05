3aaa088bbb265e853e1e100affe01abbc5a1fe68
Robert-96 <dezmereanrobert@gmail.com>
2025-01-05 01:37:27 +0000

    Refactor font properties to use a base path for improved readability and maintainability

--- src/style.py
+++ src/style.py
@@ -1,16 +1,20 @@
+from pathlib import Path
+
 import matplotlib.font_manager as font_manager
+
+FONT_BASE_PATH = Path("./fonts/static")
 
 
 class OutfitFont:
-    BLACK = font_manager.FontProperties(fname="./fonts/static/Outfit-Black.ttf", weight=900)
-    EXTRA_BOLD = font_manager.FontProperties(fname="./fonts/static/Outfit-ExtraBold.ttf", weight=800)
-    BOLD = font_manager.FontProperties(fname="./fonts/static/Outfit-Bold.ttf", weight=700)
-    SEMI_BOLD = font_manager.FontProperties(fname="./fonts/static/Outfit-SemiBold.ttf", weight=600)
-    MEDIUM = font_manager.FontProperties(fname="./fonts/static/Outfit-Medium.ttf", weight=500)
-    REGULAR = font_manager.FontProperties(fname="./fonts/static/Outfit-Regular.ttf", weight=400)
-    LIGHT = font_manager.FontProperties(fname="./fonts/static/Outfit-Light.ttf", weight=300)
-    EXTRA_LIGHT = font_manager.FontProperties(fname="./fonts/static/Outfit-ExtraLight.ttf", weight=200)
-    THIN = font_manager.FontProperties(fname="./fonts/static/Outfit-ExtraLight.ttf", weight=100)
+    BLACK = font_manager.FontProperties(fname=FONT_BASE_PATH / "Outfit-Black.ttf", weight=900)
+    EXTRA_BOLD = font_manager.FontProperties(fname=FONT_BASE_PATH /  "/Outfit-ExtraBold.ttf", weight=800)
+    BOLD = font_manager.FontProperties(fname=FONT_BASE_PATH / "Outfit-Bold.ttf", weight=700)
+    SEMI_BOLD = font_manager.FontProperties(fname=FONT_BASE_PATH / "Outfit-SemiBold.ttf", weight=600)
+    MEDIUM = font_manager.FontProperties(fname=FONT_BASE_PATH / "Outfit-Medium.ttf", weight=500)
+    REGULAR = font_manager.FontProperties(fname=FONT_BASE_PATH / "Outfit-Regular.ttf", weight=400)
+    LIGHT = font_manager.FontProperties(fname=FONT_BASE_PATH / "Outfit-Light.ttf", weight=300)
+    EXTRA_LIGHT = font_manager.FontProperties(fname=FONT_BASE_PATH / "Outfit-ExtraLight.ttf", weight=200)
+    THIN = font_manager.FontProperties(fname=FONT_BASE_PATH / "Outfit-ExtraLight.ttf", weight=100)
 
 
 class Colors:
